312502b038d37ff163ce2a251a851c96c5cf5ccd
Juan Sandoval <correo.juanm.sandoval@gmail.com>
2022-10-23 02:48:44 -0300

    Cambio de .then() a await
    get matchs ya no usa .then() y ahora usa await. para evitar problemas. No ha cambiado en los demas

--- backend/routes/matchs.js
+++ backend/routes/matchs.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-<<<<<<< Updated upstream
+
 var db = require('../matchs.js');
 /**
  *      /matchs
@@ -11,31 +11,23 @@ var db = require('../matchs.js');
  *      GET http://localost:3000/matchs Obtener las matchs que se estan trabajando o trabajaron
  *      Parametros: id (opcional) - Regresa una match por su id
  */
-router.get('/', function(req, res, _){ 
-    if(req.query.id == null){
-        db.find().then(result =>{
-            res.json(result);
-            console.log("GET match id="+req.query.id)
-            console.log(result);
-        });
+router.get('/', async(req, res, _)=>{ 
+    if(req.query.id == null){     
+        res.json(await db.find());
+        console.log("GET match id="+req.query.id)
     }
-    else{
-        db.find().then(result =>{
-            res.json(result);
-            console.log(result +"\nid="+req.query.id)
-        });
+    else{   
+        res.json(await db.find().then);
     }
 });
 
 module.exports = router;
-=======
 router.post('/', function (req, res) {
     /* parametros posibles
         id: La id de la match
 
     */
 });
-router.get('/', function(req, res, _){
+router.put('/', function(req, res, _){
 });
 module.exports = router
->>>>>>> Stashed changes
