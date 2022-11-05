7238a8c9144858fd36938239eab38aab2cbe7981
Juan Sandoval <correo.juanm.sandoval@gmail.com>
2022-11-05 18:08:09 -0300

    module exportsa

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
+  transpileDependencies: true
+  
+})
+module.exports = {
   devServer: {
     disableHostCheck: true
   }
-})
+}
