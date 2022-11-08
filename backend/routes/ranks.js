73a39fe6547c21b29ba52bf0f906d5735707df48
Juan Sandoval <correo.juanm.sandoval@gmail.com>
2022-11-08 12:26:11 -0300

    rank send images

--- backend/routes/ranks.js
+++ backend/routes/ranks.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../ranks.js');
+const path = require('path');
 /**
  *      /ranks
  *      Obtener los ranks disponibles en el juego.
@@ -26,6 +27,29 @@ router.get('/', function(req, res, _){
         db.find().then(result =>{
             res.json(result);
             console.log(result +"\nname="+req.query.name)
+        });
+    }
+    
+});
+router.get('/image', async(req, res, _) =>{
+
+    if(req.query.name != null){
+        req.query.name = req.query.name.replace("-"," ")
+        if(typeof req.query.name !== 'string'){
+            res.send('Debe entregar un id valida')
+            return
+        }
+        db.findRank(req.query.name).then(result =>{
+            console.log('result.img es '+ result.img)
+            res.sendFile(result.img,{ root: path.join(__dirname, '../img') })
+            console.log('imagen enviada')
+        });
+    }
+    else{
+        db.find().then(result =>{
+            res.json(result);
+            console.log(result +"\nname="+req.query.name)
+            console.log('LA CONCHAAA')
         });
     }
     
