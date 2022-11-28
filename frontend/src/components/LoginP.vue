555b15b14019db7bc6bdb4d0e1cfe8a1b550bdb4
Juan Sandoval <correo.juanm.sandoval@gmail.com>
2022-11-28 07:14:04 -0300

    Login y register incorporados earlie v
    Se está implementando login y register, funcionan faltan restricciones y mensajes de error

--- frontend/src/components/LoginP.vue
+++ frontend/src/components/LoginP.vue
@@ -26,7 +26,7 @@
                         <el-form-item>
                             <el-input
                                 placeholder="ContraseÃ±a"
-                                v-model="formLabelAlign.pasword"
+                                v-model="formLabelAlign.password"
                                 type="password"
                                 autocomplete="off"
                             />
@@ -34,14 +34,13 @@
                         <el-form-item>
                             <el-row :gutter="90">
                                 <el-col span="1">
-                                    <el-button color="#f5447e" type="primary">Iniciar sesion</el-button>   
+                                    <el-button color="#f5447e" type="primary" @click="login">Iniciar sesion</el-button>   
                                 </el-col>
                                 <el-col span="3">
-                                    <el-button  color="#f5447e" type="primary">Registrarse</el-button>
+                                    <el-button  color="#f5447e" type="primary" @click="register">Registrarse</el-button>
                                 </el-col>
                             </el-row>
                         </el-form-item>
-                        
                     </el-form>
             </el-col>
         </el-row>
@@ -51,11 +50,70 @@
 </template>
 <script setup>
 import { reactive} from 'vue'
-
+import { ElMessage, ElMessageBox } from 'element-plus'
+import { h } from 'vue'
+import axios from 'axios';
+import router from '../router'
 const formLabelAlign = reactive({
   name: '',
-  pasword: '',
+  password: '',
 })
+
+const login = async() =>{
+    console.log('name es '+formLabelAlign.name+ "  y pass es " +formLabelAlign.password  )
+    var loginData = (await axios.post('http://localhost:3000/users/login',
+        //body
+        {
+            name: formLabelAlign.name,
+            password: formLabelAlign.password 
+        }
+        //endbody
+    )).data;
+    console.log(loginData);
+    ElMessageBox({
+        title: 'Logueado!',
+        message: h('p', null, [
+        h('span', null, 'Ha sido logueado con '),
+        h('i', { style: 'color: green' }, 'Exito'),
+        ]),
+        confirmButtonText: 'OK',
+        beforeClose: (action, instance, done) => {
+        if (action === 'confirm') {
+            instance.confirmButtonLoading = true
+            instance.confirmButtonText = 'volviendo...'
+            setTimeout(() => {
+                done()
+                router.push( { path: '/'})
+            }, 1000)
+        } else {
+            done()
+        }
+        },
+    }).then(() => {
+        ElMessage({
+        type: 'success',
+        message: `Volviendo a la página principal`,
+    })
+  })
+}
+
+const register = async() =>{
+    console.log('name es '+formLabelAlign.name+ "  y pass es " +formLabelAlign.password  )
+    var res = (await axios.post('http://localhost:3000/users',
+        //body
+        {
+            name: formLabelAlign.name,
+            password: formLabelAlign.password 
+        }
+        //endbody
+    )).data;
+    console.log(res);
+    ElMessageBox({
+        title: 'Registrado!',
+        message: 'Puede loguearse :v',
+        confirmButtonText: 'OK'
+    })
+}
 </script>
 <style>
 img{
