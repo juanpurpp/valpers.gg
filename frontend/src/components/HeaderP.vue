d67582dc21a330fb4167d02d07ce88ffb208dcba
Juan Sandoval <correo.juanm.sandoval@gmail.com>
2022-11-28 08:34:49 -0300

    button change pos

--- frontend/src/components/HeaderP.vue
+++ frontend/src/components/HeaderP.vue
@@ -1,17 +1,14 @@
 <template> 
-<!--Parte superior de la pagina-->
-  <el-row  :align="top">
-    <el-col :span="1">
-      <img style="width: 247px; height: 70px" src="@/assets/logo.png"/>
-    </el-col>
-   <el-col>
-    <div class="grid-content bg-puerple-dark">
-      <el-button type="primary" color="#f5447e" @click="$router.push('/login')">Login</el-button>
-      <router-view/>  
-    </div>
-   </el-col>
-  </el-row>
-</template>
+  <!--Parte superior de la pagina-->
+    <el-row class="row-bg" justify="space-between">
+      <el-col :span="6">
+        <img style="width: 247px; height: 70px" src="@/assets/logo.png"/>
+      </el-col>
+      <el-col :span="6">
+          <el-button type="primary" color="#f5447e" @click="$router.push('/login')">Login</el-button>
+      </el-col>
+    </el-row>
+  </template>
 
 <script>
 export default {
