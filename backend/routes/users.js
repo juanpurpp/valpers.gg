d5cf796820af190bc0cafbd28bc640a1558cff6f
Juan Sandoval <correo.juanm.sandoval@gmail.com>
2022-11-28 05:20:43 -0300

    Logeo y autorización añadidos
    Se añadió el logeo y la autorización

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,11 +1,12 @@
 const { signedCookie } = require('cookie-parser');
 var express = require('express');
 var router = express.Router();
-//var authorize = require('../controllers/auth_controller.js')
+var jwt = require('jsonwebtoken')
+var authorize = require('../controllers/auth_controller.js')
 var db = require('../users.js');
-/*router.get('/', authorize(['admin']), function(req, res, next) {
+router.get('/', authorize(), function(req, res, next) {
   res.send('authorized user');
-});*/
+});
 
 router.post('/', async(req,res) => {
   try{ db.add({"name": req.body.name,"password": req.body.password, "roles": "usuario"});
@@ -15,4 +16,19 @@ router.post('/', async(req,res) => {
   }
   res.send('Usuario creado :v')
 } );
+router.post('/login', function(req, res) {
+  console.log(req.body)
+  db.findOne(req.body.name)
+ .then((user => {
+   if (!user || !user.comparePassword(req.body.password)) {
+       return res.status(401).json({ message: 'Usuario o contraseña erroneos' });
+   }
+   const token = jwt.sign({ email: user.email, user_id: user._id, roles: user.roles}, process.env.JWT_KEY, {expiresIn: "7d"})
+   user.token = token
+   return res.status(200).json({ token: token, email: user.email, roles: user.roles, id: user._id});
+ }))
+ .catch ((error) => {
+  console.log(error) 
+})
+});
 module.exports = router;
